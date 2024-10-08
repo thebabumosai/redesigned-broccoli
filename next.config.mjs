e68250b930e1ad8e504199d5b424c558f552e24b
thebabumosai <thebabumosai@gmail.com>
2024-10-08 22:50:24 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,11 @@ const nextConfig = {
           bodySizeLimit: '6mb',
         },
       },
+      eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+      },
 };
 
 export default nextConfig;
